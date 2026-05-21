1f0ab76e3271a8e9f58d8a79da3c319e7e2d855f
Jeet <jeet.11k@gmail.com>
2026-05-22 01:31:22 +0530

    first commit

--- script1.py
+++ script1.py
@@ -1,5 +1,7 @@
 # Write a Python program that asks the user to enter their birth year and calculates their age in the year 2026. Then display the calculated age.
+from datetime import datetime
 birth_year = input("Birth Year: ")
-age = 2026 - int(birth_year)
+current_year = datetime.now().year
+age = current_year - int(birth_year)
 print(age)
 
